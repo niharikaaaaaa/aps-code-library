49f728e3744e51414082c9fff53c2236c2489bcf
Niharika K <79542776+niharikaaaaaa@users.noreply.github.com>
2023-04-28 14:37:21 +0530

    Add files via upload

---
+++ 53-NextGreaterElement.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    vector < int > nextGreaterElements(vector < int > & nums) {
+      int n = nums.size();
+      vector < int > nge(n, -1);
+      stack < int > st;
+      for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= nums[i % n]) {
+          st.pop();
+        }
+
+        if (i < n) {
+          if (!st.empty()) nge[i] = st.top();
+        }
+        st.push(nums[i % n]);
+      }
+      return nge;
+    }
+};
+int main() {
+  Solution obj;
+  int n;
+  cin>>n;
+  vector<int>v(n);
+  for(int i=0;i<n;i++)
+  {
+    cin>>v[i];
+  }
+  vector < int > res = obj.nextGreaterElements(v);
+  for (int i = 0; i < res.size(); i++) {
+    cout << res[i] << " ";
+  }
+}
