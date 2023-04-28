83a4eb091cea23b63f981c8f0162a8ba0b6c93c1
Niharika K <79542776+niharikaaaaaa@users.noreply.github.com>
2023-04-28 13:34:39 +0530

    Add files via upload

---
+++ 41-GridUniquePaths.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+            int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
+    }
+};
+int main()
+{
+    Solution obj;
+    int m,n;
+    cin>>m>>n;
+    int totalCount= obj.uniquePaths(m,n);
+    cout<<totalCount<<endl;
+}
