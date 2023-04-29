0f2e2a199ecc21080cfb7980cfe0a48375a51e5a
Niharika K <79542776+niharikaaaaaa@users.noreply.github.com>
2023-04-29 14:34:00 +0530

    Add files via upload

---
+++ 66-RotatedArray.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int search(vector < int > & nums, int target) {
+  int low = 0, high = nums.size() - 1; //<---step 1
+
+  while (low <= high) { //<--- step 2
+    int mid = (low + high) >> 1; //<----step 3
+    if (nums[mid] == target)
+      return mid; // <---step 4
+
+    if (nums[low] <= nums[mid]) { //<---step 5
+      if (nums[low] <= target && nums[mid] >= target)
+        high = mid - 1; //<---step 6 
+      else
+        low = mid + 1; //<---step 7
+    } else { //<---step 7
+      if (nums[mid] <= target && target <= nums[high])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  }
+  return -1; //<---step 8
+}
+
+int main() 
+{
+ int n,target;
+ cin>>n>>target;
+ vector<int>nums(n);
+ for(int i=0;i<n;i++)
+ {
+  cin>>nums[i];
+ }
+  cout << search(nums, target);
+
+  return 0;
+}
