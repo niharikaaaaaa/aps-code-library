cab3aa37261885075f7fd979ef1513072cabb0a7
Niharika K <79542776+niharikaaaaaa@users.noreply.github.com>
2023-04-29 14:58:09 +0530

    Add files via upload

---
+++ 74-LargestInHistogram.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    int largestRectangleArea(vector < int > & histo) {
+      stack < int > st;
+      int maxA = 0;
+      int n = histo.size();
+      for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+          int height = histo[st.top()];
+          st.pop();
+          int width;
+          if (st.empty())
+            width = i;
+          else
+            width = i - st.top() - 1;
+          maxA = max(maxA, width * height);
+        }
+        st.push(i);
+      }
+      return maxA;
+    }
+};
+int main() 
+{
+  int n,cows;
+  cin>>n>>cows;
+  vector<int>histo(n);
+  for(int i=0;i<n;i++)
+  {
+    cin>>histo[i];
+  }
+  Solution obj;
+  cout << obj.largestRectangleArea(histo) << endl;
+  return 0;
+}
